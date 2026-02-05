4d71b3d5ac32f7febd0df807955751182e6bb02d
cristianfernando62 <l25550026@chihuahua2.tecnm.mx>
2026-02-04 19:20:26 -0600

    Add files via upload

---
+++ EVA1_3_ACCESO_DEFAULT.java
@@ -0,0 +1,13 @@
+package eva1_3_acceso_default;
+
+import OTRO_PAQUETE.Persona;
+
+public class EVA1_3_ACCESO_DEFAULT {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Persona persona = new Persona();// instalacion --> crear el objeto
+        persona
+    }
+    
+}
