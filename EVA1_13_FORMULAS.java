af9d14ff9eca091749845b5224c27bf00f292764
cristianfernando62 <l25550026@chihuahua2.tecnm.mx>
2026-02-25 14:51:04 -0600

    Add files via upload

---
+++ EVA1_13_FORMULAS.java
@@ -0,0 +1,13 @@
+
+package eva1_13_formulas;
+public class EVA1_13_FORMULAS {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+   Geometria geo = new Geometria(0);
+   geo.areacuadrado(15.5);
+   geo.areacirculo(20, true);
+     geo.areatriangulo(34, 65);
+    }
+    
+}
